998d259ecf62000343db389eb3af1edff2138ec1
DIMPHO290 <dimphotshidi1@gmail.com>
2024-06-25 12:43:29 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,17 @@
+
+package basicframeapp;
+
+import za.ac.tut.ul.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+   
+    public static void main(String[] args) {
+      //Declare  variable
+      MyFirstFrame  myFrame;
+      myFrame  = new MyFirstFrame();
+      
+    }
+    
+}
